fcb5ec4a3c11f5d99e004a2c67545a4802423283
MJ20027 <mi02code@gmail.com>
2023-07-16 11:29:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function img1Link(){
+    alert("you clicked in img 1");
+}
+function img2Link(){
+    alert("you clicked in img 2");
+}
+function img3Link(){
+    alert("you clicked in img 3");
+}
+function img4Link(){
+    alert("you clicked in img 4");
+}
+function img5Link(){
+    alert("you clicked in img 5");
+}
+function img6Link(){
+    alert("you clicked in img 6");
+}
+
